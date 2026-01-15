a90cfcc04d8c26e0c1579e8de3e81a0fccc7a08c
aniltparameswaran <aniltparameswaran@gmail.com>
2026-01-15 18:19:11 +0530

    update

--- VoiceFirst_Admin.Utilities/DTOs/Shared/StatusChangeDto.cs
+++ VoiceFirst_Admin.Utilities/DTOs/Shared/StatusChangeDto.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
-namespace VoiceFirst_Admin.Utilities.DTOs.Shared
-{
-    public class StatusChangeDto
-    {
-        [Required]
-        public int Id { get; set; }
+namespace VoiceFirst_Admin.Utilities.DTOs.Shared;
 
-        [Required]
-        public bool Status { get; set; }
-    }
+public class StatusChangeDto
+{
+    [Required]
+    public int Id { get; set; }
+
+    [Required]
+    public bool Status { get; set; }
 }
+
--- VoiceFirst_Admin.Utilities/Models/Base/BaseModel.cs
+++ VoiceFirst_Admin.Utilities/Models/Base/BaseModel.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace VoiceFirst_Admin.Utilities.Models.Base
-{
-    public class BaseModel
-    {
-        public bool IsActive { get; set; }
+namespace VoiceFirst_Admin.Utilities.Models.Base;
 
-        public int CreatedBy { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public int? UpdatedBy { get; set; }
-        public DateTime? UpdatedAt { get; set; }
-        public bool IsDeleted { get; set; }
-        public int? DeletedBy { get; set; }
-        public DateTime? DeletedAt { get; set; }
-    }
+public class BaseModel
+{
+    public bool IsActive { get; set; }
+
+    public int CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int? UpdatedBy { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+    public int? DeletedBy { get; set; }
+    public DateTime? DeletedAt { get; set; }
 }
+
