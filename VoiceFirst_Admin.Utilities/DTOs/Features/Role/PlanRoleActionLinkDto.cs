11b3f50eb930d6c7c749dd4f8bfc9f12d8ec81bb
aniltparameswaran <aniltparameswaran@gmail.com>
2026-02-03 11:56:47 +0530

    update role class

--- VoiceFirst_Admin.Utilities/DTOs/Features/Role/PlanRoleActionLinkDto.cs
+++ VoiceFirst_Admin.Utilities/DTOs/Features/Role/PlanRoleActionLinkDto.cs
@@ -18,10 +18,5 @@ namespace VoiceFirst_Admin.Utilities.DTOs.Features.Role
         //public string DeletedUser { get; set; } = string.Empty;
         //public DateTime? DeletedDate { get; set; }
     }
-    public class PlanRoleActionLinkDetailsDto
-    {
-
-        public int PlanRoleLinkId { get; set; }
-        public List<PlanRoleActionLinkDto> PlanActionLink { get; set; }
-    }
+    
 }
